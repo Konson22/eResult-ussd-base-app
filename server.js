91b01b274b32a0fc019ec333925aff9016c81ee2
Kon Akech <76453981+Konson22@users.noreply.github.com>
2023-11-26 12:27:31 +0300

    Update server.js
    added conditional to check if used added index No or not

--- server.js
+++ server.js
@@ -12,15 +12,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // USSD handler function
 const ussdHandler = (req, res) => {
-  // Extract user input
-  const userInput = req.body.text;
+    // Extract user input
+    const userInput = req.body.text;
 
-  // Process the user input (you can add your logic here)
-  const responseMessage = `Thank you for entering student index number: ${userInput}. We will send a message to you.`;
+    let responseMessage
+    if(userInput === ''){
+        responseMessage = `Entering yout index number:`;
+    }else{
+      // Process the user input (you can add your logic here)
+        responseMessage = `Thank you We send your results to your number.`;
+    }
 
-  // Send the response to the USSD gateway
-  res.send(`CON ${responseMessage}`);
+    // Send the response to the USSD gateway
+    res.send(`CON ${responseMessage}`);
 };
+
 
 // Define USSD endpoint
 app.post('/ussd', ussdHandler);
