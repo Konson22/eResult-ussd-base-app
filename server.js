dbec1a8b6ec7a8ee778afc81d16614207f54854c
Konson Ak <konakech3@gmail.com>
2023-11-27 17:05:09 +0300

    addecd

--- server.js
+++ server.js
@@ -1,26 +1,74 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 
 const app = express();
 const port = 3000;
-app.use(cors({origin: '*'}));
+const sqlite = require('sqlite3').verbose();
+app.use(cors());
 
 // Middleware to parse the body of incoming requests
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = new sqlite.Database("./database.db", sqlite.OPEN_READWRITE, err => err && console.log(err));
+
+
+app.get('/', (req, res) => {
+    db.all('SELECT * FROM StudentData', [], (err, rows) => {
+        if(err) throw err;
+        res.json(rows)
+    })
+    // console.log('hii')
+})
 // USSD handler function
 const ussdHandler = (req, res) => {
-  // Extract user input
-  const userInput = req.body.text;
+    // Extract user input
+    const {  sessionId, serviceCode, phoneNumber, text } = req.body;
 
-  // Process the user input (you can add your logic here)
-  const responseMessage = `Thank you for entering student index number: ${userInput}. We will send a message to you.`;
+    if(text === ''){
+        return res.send(`CON Entering yout index number:`);
+        
+    }else{
+        // Process the user input (you can add your logic here)
+        db.get(`SELECT * FROM StudentData WHERE IndexNo = '${text}'`, [], (err, result) => {
+            if(err) throw err;
+            const data = `
+                ${result.Name} your results are:\n
+                - English = ${result.English} \n
+                - Mathematics = ${result.Mathematics} \n
+                - AdditionalMaths = ${result.AdditionalMaths} \n
+                - Biology = ${result.Biology} \n
+                - Physics = ${result.Physics} \n
+                - Chemistry = ${result.Chemistry} \n
+                - CRE = ${result.CRE} \n
+            `
 
-  // Send the response to the USSD gateway
-  res.send(`CON ${responseMessage}`);
+            sendMessage(phoneNumber, data)
+            // Send the response to the USSD gateway
+            return res.send(`CON Thank you We send your ${result.Name} to your number`);
+        })
+    }
 };
+
+
+const credentials = { apiKey: process.env.API_KEY, username: 'sandbox' }
+const Africastalking = require('africastalking')(credentials);
+
+function sendMessage(to, message){
+    
+    
+    const sms = Africastalking.SMS
+    
+    sms.send({to, message})
+        .then( response => {
+            console.log(response);
+        })
+        .catch( error => {
+            console.log(error);
+    });
+}
 
 // Define USSD endpoint
 app.post('/ussd', ussdHandler);
@@ -29,3 +77,41 @@ app.post('/ussd', ussdHandler);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+/*
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+    const user_input = req.body.text;
+
+    // Process user input (replace this logic with your own)
+    if (user_input && /^\d+$/.test(user_input)) {
+        const index_no = parseInt(user_input);
+        
+        // Forward SMS (replace this with your SMS sending logic)
+        sendSMS(index_no);
+        
+        res.send(`SMS forwarded successfully for index number ${index_no}`);
+    } else {
+        res.send('Invalid input. Please enter a valid index number.');
+    }
+});
+
+function sendSMS(index_no) {
+    // Replace this with your SMS sending logic
+    console.log(`Forwarded SMS for index number ${index_no}`);
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+*/
--- table.js
+++ table.js
@@ -3,21 +3,27 @@ const sqlite = require('sqlite3').verbose();
 
 const db = new sqlite.Database("./database.db", sqlite.OPEN_READWRITE, err => err && console.log(err));
 
-const sql = `CREATE TABLE results_23(
-    id INTEGER PRIMARY KEY, 
-    state TEXT NOT NULL,
-    county TEXT NOT NULL,
-    centerIndex TEXT NOT NULL,
-    studentIndex TEXT NOT NULL,
-    studentName TEXT NOT NULL,
-    eng INTEGER,
-    math INTEGER,
-    cre INTEGER,
-    islamic INTEGER,
-    physic INTEGER,
-    chemistry INTEGER,
-    biology INTEGER,
-    arabic INTEGER
-)`;
+// const sql = `CREATE TABLE IF NOT EXISTS StudentData (
+//     StudentID INTEGER PRIMARY KEY AUTOINCREMENT,
+//     Name TEXT,
+//     Gender TEXT,
+//     IndexNo TEXT,
+//     CentreNo TEXT,
+//     SchoolName TEXT,
+//     Chemistry INTEGER,
+//     Physics INTEGER,
+//     Biology INTEGER,
+//     Mathematics INTEGER,
+//     English INTEGER,
+//     CRE INTEGER,
+//     AdditionalMaths INTEGER
+// );`
+
+const sql = `INSERT INTO StudentData (Name, Gender, IndexNo, CentreNo, SchoolName, Chemistry, Physics, Biology, Mathematics, English, CRE, AdditionalMaths) VALUES
+    ('John Doe', 'Male', '123456', '789012', 'ABC High School', 85, 76, 92, 68, 77, 89, 94),
+    ('Jane Doe', 'Female', '654321', '012345', 'XYZ Secondary School', 72, 88, 56, 79, 91, 40, 63),
+    ('Alex Smith', 'Male', '987654', '543210', 'PQR Academy', 98, 83, 71, 90, 45, 55, 76);`
+
+
 
 db.run(sql)
